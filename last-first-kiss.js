f85f94eaf33759ca9ddf3d453f982e23d7c83bfc
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-10 02:39:34 +0300

    m

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,17 +1,17 @@
 function first(arg) {
-  if (typeof arg == "string" && Array.isArray(arg)) {
+  if (typeof arg == "string" || Array.isArray(arg)) {
     return arg[0]
   }
 }
 
 function last(arg) {
-  if (typeof arg == "string" && Array.isArray(arg)) {
+  if (typeof arg == "string" || Array.isArray(arg)) {
     return arg[arg.length - 1]
   }
 }
 
 function kiss(arg) {
-  if (typeof arg == "string" && Array.isArray(arg)) {
+  if (typeof arg == "string" || Array.isArray(arg)) {
     return [first(arg), last(arg)]
   }
 }
