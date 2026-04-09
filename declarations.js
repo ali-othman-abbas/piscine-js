c867d999ae65a053b6422d097d0fc957b24bdfb7
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-10 01:32:54 +0300

    m

---
+++ declarations.js
@@ -0,0 +1,16 @@
+const escapeStr = "`\\/\"'"
+const arr = [4, '2'];
+const obj = {
+  str: "hello",
+  num: 1234,
+  bool: true,
+  undef: undefined,
+}
+const nested = {
+  arr: [4, undefined, '2'],
+  obj: {
+    str: "hello",
+    num: 1234,
+    bool: true,
+  }
+}
