c35fc8565cded02e197223750ed74c5a5cb940e3
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-10 16:00:02 +0300

    this should be enough

---
+++ dog-years.js
@@ -0,0 +1,14 @@
+const ageConvs = {
+  'earth': 1.0,
+  'mercury': 0.2408467,
+  'venus': 0.61519726,
+  'jupiter': 11.862615,
+  'saturn': 29.447498,
+  'uranus': 84.016846,
+  'neptune': 164.79132 
+}
+function dogYears(planetName, dogAgeInSecs) {
+  return ((dogAgeInSecs/(31_557_600))*ageConvs[planetName]).toFixed(2)
+}
+
+console.log(dogYears('earth', 1_000_000_000))
