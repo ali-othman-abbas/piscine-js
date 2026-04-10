7ff5df0e1878fd6b7f9d53a04f88d00ede0305ea
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-11 00:47:18 +0300

    m

--- elementary.js
+++ elementary.js
@@ -1,15 +1,18 @@
 // const multiply = (a, b) => (b < 0) ? -positiveMult(a, -b) : positiveMult
 // const divide = (a, b) => (b < 0) ? -positiveDivide(a, -b) : positiveMult
-const multiply = (a, b) => (b < 0) ? positiveMult(-a, -b) : positiveMult(a, b)
-const positiveMult = (a, b) => (b > 0) ? (a + positiveMult(a, b - 1)) : 0
+const multiply = (a, b) => (b < 0 ? positiveMult(-a, -b) : positiveMult(a, b));
+const positiveMult = (a, b) => (b > 0 ? a + positiveMult(a, b - 1) : 0);
 const divide = (a, b) =>
-  (a < 0 && b < 0) ?
-    positiveDiv(-a, -b) : (a < 0) ?
-      -positiveDiv(-a, b) : (b < 0) ?
-        -positiveDiv(a, -b) : positiveDiv(a, b)
-const positiveDiv = (a, b) => (b <= a) ? 1 + positiveDiv(a - b, b) : 0
-const modulo = (a, b) => (a < 0) ? -positiveMod(-a, Math.abs(b)) : positiveMod(a, Math.abs(a))
-const positiveMod = (a, b) => (b <= a) ? positiveMod(a - b, b) : a
+  a < 0 && b < 0
+    ? positiveDiv(-a, -b)
+    : a < 0
+      ? -positiveDiv(-a, b)
+      : b < 0
+        ? -positiveDiv(a, -b)
+        : positiveDiv(a, b);
+const positiveDiv = (a, b) => (b <= a ? 1 + positiveDiv(a - b, b) : 0);
+const modulo = (a, b) =>
+  a < 0 ? -positiveMod(-a, Math.abs(b)) : positiveMod(a, Math.abs(b));
+const positiveMod = (a, b) => (b <= a ? positiveMod(a - b, b) : a);
 
-
-console.log(divide(200, 78))
+console.log(modulo(34, 78));
