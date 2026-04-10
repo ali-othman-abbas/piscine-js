404e505d72e63ed6402b10a6f0b81056b7d7f9d3
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-11 00:38:48 +0300

    m

--- elementary.js
+++ elementary.js
@@ -1,7 +1,7 @@
 // const multiply = (a, b) => (b < 0) ? -positiveMult(a, -b) : positiveMult
 // const divide = (a, b) => (b < 0) ? -positiveDivide(a, -b) : positiveMult
-const multiply = (a, b) => (b < 0) ? multiply(-a, -b) : multiply(a, b)
-const positiveMult = (a, b) => (b > 0) ? (a + multiply(a, b - 1)) : 0
+const multiply = (a, b) => (b < 0) ? positiveMult(-a, -b) : positiveMult(a, b)
+const positiveMult = (a, b) => (b > 0) ? (a + positiveMult(a, b - 1)) : 0
 const divide = (a, b) => (a < 0 && b < 0) ? positiveDiv(-a, -b) :
   (a < 0) ? -positiveDiv(-a, b) : -positiveDiv(a, -b)
 const positiveDiv = (a, b) => (b <= a) ? 1 + positiveDiv(a - b, b) : 0
@@ -9,3 +9,4 @@ const modulo = (a, b) => (a < 0) ? -positiveMod(-a, Math.abs(b)) : positiveMod(a
 const positiveMod = (a, b) => (b <= a) ? positiveMod(a - b, b) : a
 
 
+console.log(multiply(34, 78))
