0f9800ab3d8d6bd135d018a38867a2924011557a
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-10 17:38:22 +0300

    m

--- min-max.js
+++ min-max.js
@@ -5,7 +5,7 @@ function max(num1, num2) {
   
   return (num1 > num2) ? num1 : num2
 }
-function max(num1, num2) {
+function min(num1, num2) {
   if (typeof num1 != 'number' || typeof num2 != 'number') {
     return
   }
