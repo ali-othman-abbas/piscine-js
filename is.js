ddf6ce7f0d1f8eb5c9292f0d243fc04827869fc7
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-10 19:02:09 +0300

    m

--- is.js
+++ is.js
@@ -4,7 +4,7 @@ is.str = (v) => typeof v === 'string'
 is.bool = (v) => typeof v === 'boolean'
 is.undef = (v) => v === undefined
 is.arr = (v) => Array.isArray(v)
-is.obj = (v) => typeof v === 'object'
+is.obj = (v) => !is.arr(v) && typeof v === 'object'
 is.fun = (v) => typeof v === 'function'
 is.truthy = (v) => !!v
-is.falsy = (v) => !!v
+is.falsy = (v) => !v
