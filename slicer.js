86c89964ddf4aa1a5051d559717af88c3d5400e1
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-11 01:45:41 +0300

    m

--- slicer.js
+++ slicer.js
@@ -1,8 +1,10 @@
 const slice = (str, start, end = str.length) =>
   (end > str.length)
     ? slice(str, start, str.length)
-    : start < 0
-      ? slice(str, Math.max(str.length + start, 0), end)
-      : start < end
-        ? str[start] + slice(str, start + 1, end)
-        : ""
+    : end < 0
+      ? slice(str, start, Math.max(str.length + end, 0))
+      : start < 0
+        ? slice(str, Math.max(str.length + start, 0), end)
+        : start < end
+          ? str[start] + slice(str, start + 1, end)
+          : ""
