7e139fc2f569362fc368ab3a1678dff337bac0e0
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-10 17:31:13 +0300

    m

---
+++ abs.js
@@ -0,0 +1,13 @@
+function isPositive(number) {
+  if (number >= 0) return true
+  
+  return false
+}
+
+function abs(number) {
+  if (number < 0) {
+    return -1 * number
+  }
+  
+  return number
+}
