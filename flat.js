03274d0320bf191f933592ea42e19967c8f80064
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-11 19:50:17 +0300

    m

--- flat.js
+++ flat.js
@@ -1,7 +1,7 @@
-const flat = (arr, result = []) => {
+const flat = (arr, depth = 1, result = []) => {
   for (const subArr of arr) {
-    if (Array.isArray(subArr)) {
-      flat(subArr, result)
+    if (Array.isArray(subArr) && depth > 0) {
+      flat(subArr, depth - 1, result)
     } else {
       result.push(subArr)
     }
