723b9463c990dcb21472acee1c50db97c3487b19
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-11 18:39:55 +0300

    m

--- pyramid.js
+++ pyramid.js
@@ -1,9 +1,9 @@
-const pyramid = (char, depth, i = 1, spaceNum = depth - 1, charNums = 1, result = []) => {
+const pyramid = (char, depth, i = 1, spaceNum = (depth - 1), charNums = 1, result = []) => {
   if (i == depth + 1) {
     return result.join("")
   }
   
-  const row = [..." ".repeat(spaceNum), ...char.repeat(charNums)]
+  const row = [..." ".repeat(char.length*spaceNum), ...char.repeat(charNums)]
   if (i < depth) {
     row.push('\n')
   }
