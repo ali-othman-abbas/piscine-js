3e7ffaadef51812425b7a8a7482d72d2e9b4ecc6
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-11 13:32:09 +0300

    m

---
+++ reverser.js
@@ -0,0 +1,11 @@
+const reverse = (ele) =>
+  Array.isArray(ele)
+    ? arrReverse(ele)
+    : arrReverse(Array.from(ele))
+
+const arrReverse = (arr) =>
+  arr.length === 0
+    ? []
+    : arr.length === 1
+      ? [arr[0]]
+      : [arr[arr.length - 1], ...arrReverse(arr.slice(1, -1)), arr[0]]
