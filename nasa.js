4900583593f84d3ada3b87def5f27dfe84924b49
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-11 17:09:17 +0300

    m

--- nasa.js
+++ nasa.js
@@ -1,11 +1,11 @@
 function nasa(N) {
   const result = []
   for (let i = 1; i <= N; i++) {
-    if (i % 3 && i % 5) {
+    if (i % 3 == 0 && i % 5 == 0) {
       result.push('NASA')
-    } else if (i % 3) {
+    } else if (i % 3 == 0) {
       result.push('NA')
-    } else if (i % 5) {
+    } else if (i % 5 == 0) {
       result.push('SA')
     } else {
       result.push(`${i}`)
