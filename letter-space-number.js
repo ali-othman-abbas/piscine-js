56fbadc8300346af80bf589753cb5afb3a6218af
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-11 23:47:13 +0300

    m

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,3 +1,4 @@
 function letterSpaceNumber(str) {
-  return str.match(/[a-zA-Z] [0-9](?=[^a-zA-Z0-9])/g)
+  result = str.match(/[a-zA-Z] [0-9](?=[^a-zA-Z0-9])/g)
+  return result === null ? [] : result
 }
