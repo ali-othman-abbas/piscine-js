49480e5e3636d4ca73307470e6943f3435d1487b
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-11 21:24:27 +0300

    m

--- sums.js
+++ sums.js
@@ -1,17 +1,23 @@
-function sums(num, upTo = num) {
-  if (num === 0) {
-    return [[]]
-  }
-  
-  if (upTo === 0) {
+function sums(num) {
+  if (num == 0) {
     return []
   }
-  
-  const arr1 = sums(num, upTo - 1)
-  const arr2 = sums(num - upTo, num - upTo)
-  for (let i = 0; i < arr2.length; i++) {
-    arr2[i].push(upTo)
+  const inner = (num, upTo = num) => {
+    if (num === 0) {
+      return [[]]
+    }
+    
+    if (upTo === 0) {
+      return []
+    }
+    
+    const arr1 = sums(num, upTo - 1)
+    const arr2 = sums(num - upTo, num - upTo)
+    for (let i = 0; i < arr2.length; i++) {
+      arr2[i].push(upTo)
+    }
+    
+    return [...arr1, ...arr2]
   }
-  
-  return [...arr1, ...arr2]
+  inner(num)
 }
