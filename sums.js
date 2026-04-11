7f7bdb1da27096f605935f4f0b5d1275fb2748c9
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-11 21:10:53 +0300

    m

---
+++ sums.js
@@ -0,0 +1,21 @@
+function sums(num, upTo = num) {
+  if (num === 0) {
+    return [[]]
+  }
+  
+  if (upTo === 0) {
+    return []
+  }
+  
+  const arr1 = sums(num, upTo - 1)
+  const arr2 = sums(num - upTo, num - upTo)
+  for (let i = 0; i < arr2.length; i++) {
+    arr2[i].push(upTo)
+  }
+  
+  return [...arr1, ...arr2]
+}
+
+result = sums(4)
+
+console.log(result)
