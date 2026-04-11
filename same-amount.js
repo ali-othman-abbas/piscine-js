70c3a74330baa10e6c37ff2da0a777ef05020d24
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-12 00:54:10 +0300

    m

--- same-amount.js
+++ same-amount.js
@@ -1,7 +1,11 @@
 function sameAmount(str, regex1, regex2) {
-    var regex1G = (regex1.global) ? regex1 : new RegExp(regex1.source, regex1.flags + 'g');
-    var regex2G = (regex2.global) ? regex2 : new RegExp(regex2, regex2.flags + 'g');
-    var matches1 = Array.from(str.matchAll(regex1G)).length;
-    var matches2 = Array.from(str.matchAll(regex2G)).length;
-    return matches1 === matches2;
+    function countMatches(str, regex) {
+        var regexG = (regex.global) ? regex : new RegExp(regex.source, regex.flags + 'g');
+        var count = 0;
+        while (regexG.exec(str) !== null) {
+            count++;
+        }
+        return count;
+    }
+    return countMatches(str, regex1) === countMatches(str, regex2);
 }
