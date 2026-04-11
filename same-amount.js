28b0afcecea7132ae09e8ac85464fcd11b3e7bbe
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-12 00:48:07 +0300

    m

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,7 @@
 function sameAmount(str, regex1, regex2) {
-    var matches1 = Array.from(str.matchAll(regex1)).length;
-    var matches2 = Array.from(str.matchAll(regex2)).length;
+    var regex1G = (regex1.global) ? regex1 : new RegExp(regex1.source, regex1.flags + 'g');
+    var regex2G = (regex2.global) ? regex2 : new RegExp(regex2, regex2.flags + 'g');
+    var matches1 = Array.from(str.matchAll(regex1G)).length;
+    var matches2 = Array.from(str.matchAll(regex2G)).length;
     return matches1 === matches2;
 }
--- same-amount.ts
+++ same-amount.ts
@@ -1,5 +1,8 @@
 function sameAmount(str: string, regex1: RegExp, regex2: RegExp) {
-  const matches1 = Array.from(str.matchAll(regex1)).length
-  const matches2 = Array.from(str.matchAll(regex2)).length
+  const regex1G = (regex1.global) ?  regex1 : new RegExp(regex1.source, regex1.flags + 'g')
+  const regex2G = (regex2.global) ?  regex2 : new RegExp(regex2, regex2.flags + 'g')
+  
+  const matches1 = Array.from(str.matchAll(regex1G)).length
+  const matches2 = Array.from(str.matchAll(regex2G)).length
   return matches1 === matches2
 }
