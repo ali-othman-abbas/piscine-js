51907abf3b61f5238dc98574a624be870f8790aa
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-12 00:00:39 +0300

    m

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,4 +1,4 @@
 function letterSpaceNumber(str) {
-    var result = str.match(/[a-zA-Z] [0-9](?=[^a-zA-Z0-9])/g);
+    var result = str.match(/[a-zA-Z] [0-9](?=[^a-zA-Z0-9]|$)/g);
     return result === null ? [] : result;
 }
--- letter-space-number.ts
+++ letter-space-number.ts
@@ -1,4 +1,4 @@
 function letterSpaceNumber(str: string) {
-  let result = str.match(/[a-zA-Z] [0-9](?=[^a-zA-Z0-9])/g)
+  let result = str.match(/[a-zA-Z] [0-9](?=[^a-zA-Z0-9]|$)/g)
   return result === null ? [] : result
 }
