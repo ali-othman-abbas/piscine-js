66a96d39abc59542490a72b2820979736e1a2048
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-11 13:22:53 +0300

    m

--- cut-corners.js
+++ cut-corners.js
@@ -47,9 +47,3 @@ function trunc(num) {
   
   return sign * result
 }
-
-const nums = [3.7, -3.7, 3.1, -3.1]
-console.log(nums.map(round))
-console.log(nums.map(floor))
-console.log(nums.map(trunc))
-console.log(nums.map(ceil))
