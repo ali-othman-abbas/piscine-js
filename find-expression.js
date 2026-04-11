8d3f6a0bf22cf4d048b1f560ba664f9895796a0a
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-11 20:14:45 +0300

    m

---
+++ find-expression.js
@@ -0,0 +1,20 @@
+const add4 = '+4'
+const mul2 = '*2'
+const findExpression = (number, num = 1, result = ['1']) => {
+  if (number === num) {
+    return result.join("")
+  }
+  if (num > number) return
+  result.push(mul2)
+  const first = findExpression(number, num * 2, result)
+  if(first) return first
+  result.pop()
+  result.push(add4)
+  const second = findExpression(number, num + 4, result)
+  if(second) return second
+  result.pop(add4)
+  return
+}
+
+solution = findExpression(100)
+console.log(solution)
