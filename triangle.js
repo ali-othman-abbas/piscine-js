54408240ee61805c1a78a626449d7638a2750556
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-11 17:51:08 +0300

    m

--- triangle.js
+++ triangle.js
@@ -1,13 +1,15 @@
-const triangle = (depth, i = 1, result = []) => {
+const triangle = (char, depth, i = 1, result = []) => {
   if (i === depth + 1) {
     return result.join("")
   }
 
+  row = [...char.repeat(i)]
   if (i < depth) {
-    result.push([...'*'.repeat(i), '\n'].join(""))
-  } else {
-    result.push([...'*'.repeat(i)].join(""))
+    row.push("\n")
   }
+  result.push(row.join(""))
   
   return triangle(depth, i + 1, result)
 }
+
+console.log(triangle('#', 4))
