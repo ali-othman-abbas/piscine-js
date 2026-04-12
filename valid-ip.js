2325d34a4f748d42f7e7069fd62dc4db8fc14f15
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-12 18:47:33 +0300

    m

--- valid-ip.js
+++ valid-ip.js
@@ -1,6 +1,6 @@
 function findIP(str) {
   const ip = /(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}/
   const port = /:(?:6553[0-5]|655[0-2]\d|65[0-4]\d{2}|6[0-4]\d{3}|[1-5]\d{4}|[1-9]\d{1,3}|\d)/
-  const combined = new RegExp(`${ip.source}(${port.source})?`, 'g')
+  const combined = new RegExp(`(?<=\s|^)${ip.source}(${port.source})?(?=\s|$)`, 'g')
   return str.match(combined)
 }
