ebe1a34c928417887d78b7265d43e3b75a8c6f3b
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-12 15:43:49 +0300

    m

--- greedy-url.ts
+++ greedy-url.ts
@@ -1,14 +1,16 @@
+const regex = /https?:\/\/[^\s]+/g
 function getURL(str: string): Array<string> {
-  const regex = /https?:\/\/[^\s]+/g
   return str.match(regex) ?? []
 }
 
 function greedyQuery(str: string): Array<string> {
-  const regex = /https?:\/\/[^\s]+/g
-  return str.match(regex) ?? []
+  const parameters = /\?([^\s]+=[^\s]+&){2,}[^\s]+=[^\s]+/
+  const combined = new RegExp(`${regex.source}${parameters.source}`, 'g')
+  return str.match(combined) ?? []
 }
 
 function notSoGreedy(str: string): Array<string> {
-  const regex = /https?:\/\/[^\s]+/g
-  return str.match(regex) ?? []
+  const parameters = /\?(?:[^\s]+=[^\s]+&){1,2}[^\s]+=[^\s]+/
+  const combined = new RegExp(`${regex.source}${parameters.source}`, 'g')
+  return str.match(combined) ?? []
 }
