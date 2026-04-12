dc83c6477c7cc637880496dd224e11b10226bc07
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-12 13:19:07 +0300

    m

--- ion-out.js
+++ ion-out.js
@@ -1,4 +1,7 @@
 function ionOut(str) {
-  const result = str.match(/\b[a-zA-Z]*t(?=ion)\b/g) ?? []
+  const result = str.match(/\b[a-zA-Z]*t(?=ion\b)/g) ?? []
   return result
 }
+
+const result = ionOut('attention, direction')
+console.log(result)
