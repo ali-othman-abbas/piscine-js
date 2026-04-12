e5c6d03cd679bb08c86e27e1003ad7928d8a6c33
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-12 15:39:01 +0300

    m

--- greedy-url.js
+++ greedy-url.js
@@ -1,20 +1,15 @@
-var authority = /(?:[\w-]+\.)+[\w-]+/;
-var path = /(?:\/[\w-.@]+)*/;
 function getURL(str) {
     var _a;
-    var parameters = /(?:\?(?:[\w-]+=[\w-]+&)*[\w-]+=[\w-]+)?/;
-    var regex = new RegExp("https?://".concat(authority.source).concat(path.source).concat(parameters.source), 'g');
+    var regex = /https?:\/\/[^\s]+/g;
     return (_a = str.match(regex)) !== null && _a !== void 0 ? _a : [];
 }
 function greedyQuery(str) {
     var _a;
-    var greedyParameters = /\?(?:[\w-]+=[\w-]+&){2,}[\w-]+=[\w-]+/;
-    var regex = new RegExp("https?://".concat(authority.source).concat(path.source).concat(greedyParameters.source), 'g');
+    var regex = /https?:\/\/[^\s]+/g;
     return (_a = str.match(regex)) !== null && _a !== void 0 ? _a : [];
 }
 function notSoGreedy(str) {
     var _a;
-    var notSoGreedy = /\?(?:[\w-]+=[\w-]+&){1,2}[\w-]+=[\w-]+/;
-    var regex = new RegExp("https?://".concat(authority.source).concat(path.source).concat(notSoGreedy.source), 'g');
+    var regex = /https?:\/\/[^\s]+/g;
     return (_a = str.match(regex)) !== null && _a !== void 0 ? _a : [];
 }
--- greedy-url.ts
+++ greedy-url.ts
@@ -1,19 +1,14 @@
- const authority = /(?:[\w-]+\.)+[\w-]+/
- const path = /(?:\/[\w-]+)*\/?/
 function getURL(str: string): Array<string> {
-  const parameters = /(?:\?(?:[\w-]+=[\w-]+&)*[\w-]+=[\w-]+)?/
-  const regex = new RegExp(`https?://${authority.source}${path.source}${parameters.source}`, 'g')
+  const regex = /https?:\/\/[^\s]+/g
   return str.match(regex) ?? []
 }
 
 function greedyQuery(str: string): Array<string> {
-  const greedyParameters = /\?(?:[\w-]+=[\w-]+&){2,}[\w-]+=[\w-]+/
-  const regex = new RegExp(`https?://${authority.source}${path.source}${greedyParameters.source}`, 'g')
+  const regex = /https?:\/\/[^\s]+/g
   return str.match(regex) ?? []
 }
 
 function notSoGreedy(str: string): Array<string> {
-  const notSoGreedy = /\?(?:[\w-]+=[\w-]+&){1,2}[\w-]+=[\w-]+/
-  const regex = new RegExp(`https?://${authority.source}${path.source}${notSoGreedy.source}`, 'g')
+  const regex = /https?:\/\/[^\s]+/g
   return str.match(regex) ?? []
 }
