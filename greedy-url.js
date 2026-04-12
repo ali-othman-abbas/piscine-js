653acf01e51908e4a31e581dd333a3158a62e2f9
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-12 16:01:36 +0300

    m

--- greedy-url.js
+++ greedy-url.js
@@ -5,13 +5,13 @@ function getURL(str) {
 }
 function greedyQuery(str) {
     var _a;
-    var parameters = /\?([^\s=&]+=[^\s=&]+&){2,}[^\s=&]+=[^\s=&]+/;
+    var parameters = /\?([^\s=&]+=[^\s=&]+&){2,}[^\s=&]+=[^\s=&]+\b/;
     var combined = new RegExp("".concat(regex.source).concat(parameters.source), 'g');
     return (_a = str.match(combined)) !== null && _a !== void 0 ? _a : [];
 }
 function notSoGreedy(str) {
     var _a;
-    var parameters = /\?(?:[^\s=&]+=[^\s=&]+&){1,2}[^\s=&]+=[^\s=&]+/;
+    var parameters = /\?(?:[^\s=&]+=[^\s=&]+&){1,2}[^\s=&]+=[^\s=&]+\b/;
     var combined = new RegExp("".concat(regex.source).concat(parameters.source), 'g');
     return (_a = str.match(combined)) !== null && _a !== void 0 ? _a : [];
 }
