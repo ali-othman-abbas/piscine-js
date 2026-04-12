6f089cb418073649a42642554927fbbf61105096
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-12 16:43:02 +0300

    m

--- greedy-url.js
+++ greedy-url.js
@@ -13,6 +13,3 @@ function notSoGreedy(str) {
     var parameters = /\bhttps?:\/\/[^\s]+\?(?:[^\s=&]+=[^\s=&]+&){1,2}[^\s=&]+=[^\s=&]+(?=\s|$)/g;
     return (_a = str.match(parameters)) !== null && _a !== void 0 ? _a : [];
 }
-var s = "http://example.com/hello?you=something&something=you";
-var result = notSoGreedy(s);
-console.log(result);
