6f0650bd146028a3cddcced87366d12dfcba6470
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-16 22:51:59 +0300

    m

--- date-is.js
+++ date-is.js
@@ -1,25 +1,7 @@
-var tests = [];
-var t = function (f) { return tests.push(f); };
-// is the date a valid date?
-var invalid = function (callback, ary) {
-    if (ary === void 0) { ary = 1; }
-    for (var _i = 0, _a = [
-        "new Date('')",
-        "new Date(NaN)",
-        "''",
-        "'2013-01-01'",
-        "NaN",
-    ]; _i < _a.length; _i++) {
-        var s = _a[_i];
-        if (callback.apply(void 0, Array(ary).fill(eval(s)))) {
-            throw Error("".concat(callback.name, "(").concat(s, ") should be false"));
-        }
-    }
+var isValid = function (date) {
+    return date instanceof Date && !Number.isNaN(date.getTime());
 };
-var isValid = function (date) { return date instanceof Date && !Number.isNaN(date.getTime()); };
-var isAfter = function (date1, date2) {
-    return date1.getTime() > date2.getTime();
-};
+var isAfter = function (date1, date2) { return date1.getTime() > date2.getTime(); };
 var isBefore = function (date1, date2) {
     return date1.getTime() < date2.getTime();
 };
@@ -29,4 +11,3 @@ var isFuture = function (date) {
 var isPast = function (date) {
     return isValid(date) && isBefore(date, new Date(Date.now()));
 };
-// isValid
--- date-is.ts
+++ date-is.ts
@@ -1,14 +1,13 @@
+const isValid = (date: Date) =>
+  date instanceof Date && !Number.isNaN(date.getTime());
 
-const isValid = (date: Date) => date instanceof Date && !Number.isNaN(date.getTime())
-
-const isAfter = (date1: Date, date2: Date) =>
-  date1.getTime() > date2.getTime()
+const isAfter = (date1: Date, date2: Date) => date1.getTime() > date2.getTime();
 
 const isBefore = (date1: Date, date2: Date) =>
-  date1.getTime() < date2.getTime()
+  date1.getTime() < date2.getTime();
 
 const isFuture = (date: Date) =>
-  isValid(date) && isAfter(date, new Date(Date.now()))
+  isValid(date) && isAfter(date, new Date(Date.now()));
 
 const isPast = (date: Date) =>
-  isValid(date) && isBefore(date, new Date(Date.now()))
+  isValid(date) && isBefore(date, new Date(Date.now()));
