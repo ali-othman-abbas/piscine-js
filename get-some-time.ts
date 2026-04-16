bcabbc312746f1a8fb8a1addc07adfcf397a3e5a
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-17 00:30:35 +0300

    m

--- get-some-time.ts
+++ get-some-time.ts
@@ -2,11 +2,10 @@ function firstDayWeek(num: number, str: string) {
   const date = new Date(0)
   date.setFullYear(Number(str))
   num = num - 1
-  date.setTime(num * 7 * 24 * 60 * 60 * 1000 + date.getTime())
+  date.setTime(num * 7 * 24 * 60 * 60 * 1000 + date.getTime() -  2 * 24 * 60 * 60 * 1000)
   const day = String(date.getDate()).padStart(2,'0')
   const month = String(date.getMonth() + 1).padStart(2, '0')
   const year = date.getFullYear()
   
   return `${day}-${month}-${year}`
 }
-
