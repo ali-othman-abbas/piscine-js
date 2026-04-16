d3e517f30e6a5ea5daa26a1bb5c291506e064942
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-16 23:04:39 +0300

    m

--- date-is.ts
+++ date-is.ts
@@ -2,10 +2,27 @@ const isValid = (date: Date | number) =>
   (date instanceof Date && !Number.isNaN(date.getTime())) ||
   (typeof date === "number" && !Number.isNaN(new Date(date).getTime()));
 
-const isAfter = (date1: Date, date2: Date) => date1.getTime() > date2.getTime();
+const isAfter = (date1: Date | number, date2: Date | number) => {
+  if (typeof date1 === 'number') {
+    date1 = new Date(date1)
+  }
+  if (typeof date2 === 'number') {
+    date2 = new Date(date2)
+  }
+  
+  return date1.getTime() > date2.getTime()
+}
 
-const isBefore = (date1: Date, date2: Date) =>
-  date1.getTime() < date2.getTime();
+const isBefore = (date1: Date, date2: Date) => {
+  if (typeof date1 === 'number') {
+    date1 = new Date(date1)
+  }
+  if (typeof date2 === 'number') {
+    date2 = new Date(date2)
+  }
+  
+  return date1.getTime() < date2.getTime()
+}
 
 const isFuture = (date: Date) =>
   isValid(date) && isAfter(date, new Date(Date.now()));
