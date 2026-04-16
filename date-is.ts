a1ff599251c672c382083893099c965cfe71dbe7
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-16 22:48:50 +0300

    m

--- date-is.ts
+++ date-is.ts
@@ -1,9 +1,34 @@
-const isValid = (date: Date) => !Number.isNaN(date.getTime())
 
-const isAfter = (date1: Date, date2: Date) => date1.getTime() > date2.getTime()
+const tests = []
+const t = (f: any) => tests.push(f)
 
-const isBefore = (date1: Date, date2: Date) => date1.getTime() < date2.getTime()
+// is the date a valid date?
+const invalid = (callback: any, ary = 1) => {
+  for (const s of [
+    `new Date('')`,
+    `new Date(NaN)`,
+    `''`,
+    `'2013-01-01'`,
+    `NaN`,
+  ]) {
+    if (callback(...Array(ary).fill(eval(s)))) {
+      throw Error(`${callback.name}(${s}) should be false`)
+    }
+  }
+}
 
-const isFuture = (date: Date) => isValid(date) && isAfter(date, new Date(Date.now()))
+const isValid = (date: Date) => date instanceof Date && !Number.isNaN(date.getTime())
 
-const isPast = (date: Date) => isValid(date) && isBefore(date, new Date(Date.now()))
+const isAfter = (date1: Date, date2: Date) =>
+  date1.getTime() > date2.getTime()
+
+const isBefore = (date1: Date, date2: Date) =>
+  date1.getTime() < date2.getTime()
+
+const isFuture = (date: Date) =>
+  isValid(date) && isAfter(date, new Date(Date.now()))
+
+const isPast = (date: Date) =>
+  isValid(date) && isBefore(date, new Date(Date.now()))
+
+// isValid
