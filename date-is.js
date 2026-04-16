bf6fc655d0a33ba0f34245fe420053b001b939a7
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-16 23:12:22 +0300

    m

--- date-is.js
+++ date-is.js
@@ -2,8 +2,25 @@ var isValid = function (date) {
     return (date instanceof Date && !Number.isNaN(date.getTime())) ||
         (typeof date === "number" && !Number.isNaN(new Date(date).getTime()));
 };
-var isAfter = function (date1, date2) { return date1.getTime() > date2.getTime(); };
+var isAfter = function (date1, date2) {
+    if (typeof date1 === 'number') {
+        date1 = new Date(date1);
+    }
+    if (typeof date2 === 'number') {
+        date2 = new Date(date2);
+    }
+    return date1.getTime() > date2.getTime();
+};
 var isBefore = function (date1, date2) {
+    if (!isValid(date1) || !isValid(date2)) {
+        return false;
+    }
+    if (typeof date1 === 'number') {
+        date1 = new Date(date1);
+    }
+    if (typeof date2 === 'number') {
+        date2 = new Date(date2);
+    }
     return date1.getTime() < date2.getTime();
 };
 var isFuture = function (date) {
