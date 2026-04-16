453ae95c8352ae54f623d0b1e9c64d98aab1b0e9
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-16 20:24:46 +0300

    m

--- dr-strange.js
+++ dr-strange.js
@@ -3,7 +3,7 @@ function addWeek(date) {
     var epoch = new Date('0001-01-01T00:00:00Z');
     var timeFromEpoch = new Date(Math.abs(date.getTime() - epoch.getTime()));
     var day = Math.trunc(timeFromEpoch.getTime() / (1000 * 60 * 60 * 24));
-    console.log(dayStr[day % 14]);
+    return dayStr[day % 14];
 }
 function timeTravel(_a) {
     var date = _a.date, hour = _a.hour, minute = _a.minute, second = _a.second;
--- dr-strange.ts
+++ dr-strange.ts
@@ -4,7 +4,7 @@ function addWeek(date: Date) {
   const epoch = new Date('0001-01-01T00:00:00Z')
   const timeFromEpoch = new Date(Math.abs(date.getTime() - epoch.getTime()))
   const day = Math.trunc(timeFromEpoch.getTime()/(1000 * 60 * 60 * 24))
-  console.log(dayStr[day % 14])
+  return dayStr[day % 14]
 }
 
 function timeTravel({ date, hour, minute, second }: {
