abf0d7cdad15cc3e00c68f4f115c0092e144e1ce
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-17 00:32:54 +0300

    m

--- get-some-time.js
+++ get-some-time.js
@@ -2,7 +2,8 @@ function firstDayWeek(num, str) {
     var date = new Date(0);
     date.setFullYear(Number(str));
     num = num - 1;
-    date.setTime(num * 7 * 24 * 60 * 60 * 1000 + date.getTime() - 2 * 24 * 60 * 60 * 1000);
+    var milliInDay = 24 * 60 * 60 * 1000;
+    date.setTime(num * 7 * milliInDay + date.getTime() - 2 * milliInDay);
     var day = String(date.getDate()).padStart(2, '0');
     var month = String(date.getMonth() + 1).padStart(2, '0');
     var year = date.getFullYear();
