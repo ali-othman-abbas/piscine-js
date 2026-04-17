749aac911e9f79d948a2036475d217bd0063c779
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-17 13:25:42 +0300

    m

--- rebecca-black.js
+++ rebecca-black.js
@@ -7,3 +7,19 @@ function isWeekend(date) {
 function isLeapYear(date) {
     return date.getFullYear() % 4 === 0;
 }
+function isLastDayOfMonth(date) {
+    switch (date.getMonth()) {
+        case 0:
+        case 2:
+        case 4:
+        case 6:
+        case 7:
+        case 9:
+        case 11:
+            return date.getDate() == 31;
+        case 1:
+            return isLeapYear(date) && date.getDate() === 29 || !isLeapYear(date) && date.getDate() === 28;
+        default:
+            return date.getDate() === 30;
+    }
+}
--- rebecca-black.ts
+++ rebecca-black.ts
@@ -9,3 +9,20 @@ function isWeekend(date: Date) {
 function isLeapYear(date: Date) {
   return date.getFullYear() % 4 === 0
 }
+
+function isLastDayOfMonth(date: Date) {
+  switch(date.getMonth()) {
+    case 0:
+    case 2:
+    case 4:
+    case 6:
+    case 7:
+    case 9:
+    case 11:
+      return date.getDate() == 31
+    case 1:
+      return isLeapYear(date) && date.getDate() === 29 || !isLeapYear(date) && date.getDate() === 28
+    default:
+      return date.getDate() === 30
+  }
+}
