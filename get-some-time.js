94992733618a553710ea9dbbe79fbe2cc7ac2b30
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-17 03:20:27 +0300

    m

--- get-some-time.js
+++ get-some-time.js
@@ -1,27 +1,38 @@
 function firstDayWeek(num, str) {
-    if (num == 1) {
-        return "01-01-".concat(str);
-    }
-    var milliInDay = 24 * 60 * 60 * 1000;
-    var epochMonday = new Date(0);
-    epochMonday.setTime(epochMonday.getTime() - 3 * milliInDay);
-    var startOfYear = new Date(0);
-    startOfYear.setFullYear(Number(str));
-    var diff = Math.abs(new Date(epochMonday.getTime() - startOfYear.getTime()).getTime());
-    var daysDiff = Math.trunc(diff / milliInDay);
-    var rem = daysDiff % 7;
-    // + (daysDiff - rem) * milliInDay
-    var StartOfYearMonday = new Date(epochMonday.getTime());
-    if (epochMonday.getTime() > startOfYear.getTime()) {
-        StartOfYearMonday.setTime(StartOfYearMonday.getTime() - (daysDiff - rem + 7) * milliInDay);
-    }
-    else {
-        StartOfYearMonday.setTime(StartOfYearMonday.getTime() + (daysDiff - rem) * milliInDay);
-    }
-    num = num - 1;
-    var result = new Date(StartOfYearMonday.getTime() + num * 7 * milliInDay);
-    var day = String(result.getUTCDate()).padStart(2, "0");
-    var month = String(result.getUTCMonth() + 1).padStart(2, "0");
-    var year = String(result.getUTCFullYear()).padStart(4, "0");
-    return "".concat(day, "-").concat(month, "-").concat(year);
+  if (num == 1) {
+    return `01-01-${str}`
+  }
+  const milliInDay = 24 * 60 * 60 * 1000;
+
+  const epochMonday = new Date(0);
+  epochMonday.setTime(epochMonday.getTime() - 3 * milliInDay);
+
+  const startOfYear = new Date(0);
+  startOfYear.setFullYear(Number(str));
+
+  const diff = Math.abs(
+    new Date(epochMonday.getTime() - startOfYear.getTime()).getTime(),
+  );
+
+  const daysDiff = Math.trunc(diff / milliInDay);
+  const rem = daysDiff % 7;
+  // + (daysDiff - rem) * milliInDay
+  const StartOfYearMonday = new Date(epochMonday.getTime());
+  if (epochMonday.getTime() > startOfYear.getTime()) {
+    StartOfYearMonday.setTime(
+      StartOfYearMonday.getTime() - (daysDiff - rem + 7) * milliInDay,
+    );
+  } else {
+    StartOfYearMonday.setTime(
+      StartOfYearMonday.getTime() + (daysDiff - rem) * milliInDay,
+    );
+  }
+
+  num = num - 1;
+  const result = new Date(StartOfYearMonday.getTime() + num * 7 * milliInDay);
+  const day = String(result.getUTCDate()).padStart(2, "0");
+  const month = String(result.getUTCMonth() + 1).padStart(2, "0");
+  const year = String(result.getUTCFullYear()).padStart(4, "0");
+
+  return `${day}-${month}-${year}`;
 }
