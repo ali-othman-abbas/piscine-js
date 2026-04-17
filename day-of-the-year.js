dd533da059281b67c9d0ed6470d1dae987785dbf
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-18 00:29:23 +0300

    m

---
+++ day-of-the-year.js
@@ -0,0 +1,10 @@
+var days = [0, 1, -1, 0, 0, 1, 1, 2, 3, 3, 4, 4, 5];
+function dayOfTheYear(date) {
+    var daysNum = date.getUTCDate();
+    var previousMonths = date.getUTCMonth();
+    daysNum = daysNum + 30 * previousMonths + days[previousMonths];
+    if (date.getUTCFullYear() % 4 === 0) {
+        daysNum++;
+    }
+    return daysNum;
+}
