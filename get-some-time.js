a7238c53bc9a6556bb4e68a3d162692c58617a49
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-17 03:11:27 +0300

    m

--- get-some-time.js
+++ get-some-time.js
@@ -1,11 +1,27 @@
 function firstDayWeek(num, str) {
-    var date = new Date(0);
-    date.setFullYear(Number(str));
-    num = num - 1;
+    if (num == 1) {
+        return "01-01-".concat(str);
+    }
     var milliInDay = 24 * 60 * 60 * 1000;
-    date.setTime(num * 7 * milliInDay + date.getTime() - 2 * milliInDay);
-    var day = String(date.getDate()).padStart(2, '0');
-    var month = String(date.getMonth() + 1).padStart(2, '0');
-    var year = date.getFullYear();
+    var epochMonday = new Date(0);
+    epochMonday.setTime(epochMonday.getTime() - 3 * milliInDay);
+    var startOfYear = new Date(0);
+    startOfYear.setFullYear(Number(str));
+    var diff = Math.abs(new Date(epochMonday.getTime() - startOfYear.getTime()).getTime());
+    var daysDiff = Math.trunc(diff / milliInDay);
+    var rem = daysDiff % 7;
+    // + (daysDiff - rem) * milliInDay
+    var StartOfYearMonday = new Date(epochMonday.getTime());
+    if (epochMonday.getTime() > startOfYear.getTime()) {
+        StartOfYearMonday.setTime(StartOfYearMonday.getTime() - (daysDiff - rem + 7) * milliInDay);
+    }
+    else {
+        StartOfYearMonday.setTime(StartOfYearMonday.getTime() + (daysDiff - rem) * milliInDay);
+    }
+    num = num - 1;
+    var result = new Date(StartOfYearMonday.getTime() + num * 7 * milliInDay);
+    var day = String(result.getUTCDate()).padStart(2, "0");
+    var month = String(result.getUTCMonth() + 1).padStart(2, "0");
+    var year = String(result.getUTCFullYear()).padStart(4, "0");
     return "".concat(day, "-").concat(month, "-").concat(year);
 }
