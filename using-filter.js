7f40df08dfb0aee7db7fb4f5392a2234e5002048
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-18 12:18:24 +0300

    m

---
+++ using-filter.js
@@ -0,0 +1,33 @@
+var filterShortStateName = function (arr) {
+    return arr.filter(function (ele) { return ele.length < 7; });
+};
+var vowels = new Set(["a", "e", "i", "o", "u"]);
+var filterStartVowel = function (arr) {
+    return arr.filter(function (ele) { var _a; return vowels.has((_a = ele[0]) === null || _a === void 0 ? void 0 : _a.toLowerCase()); });
+};
+var filter5vowels = function (arr) {
+    return arr.filter(function (ele) {
+        var count = 0;
+        Array.from(ele).forEach(function (char) { return vowels.has(char.toLowerCase()) && count++; });
+        return count >= 5;
+    });
+};
+var filter1DistinctVowel = function (arr) {
+    return arr.filter(function (ele) {
+        var distinctVowls = new Set();
+        Array.from(ele).forEach(function (char) {
+            return vowels.has(char.toLowerCase()) && distinctVowls.add(char.toLowerCase());
+        });
+        return distinctVowls.size === 1;
+    });
+};
+var multiFilter = function (arr) {
+    return arr.filter(function (ele) {
+        var _a;
+        return ((_a = ele["capital"]) === null || _a === void 0 ? void 0 : _a.length) >= 8 &&
+            !vowels.has(ele["name"][0].toLowerCase()) &&
+            Array.from(ele["tag"]).filter(function (char) { return vowels.has(char.toLowerCase()); })
+                .length >= 1 &&
+            ele["region"] !== "South";
+    });
+};
---
+++ using-filter.ts
@@ -0,0 +1,35 @@
+const filterShortStateName = (arr: string[]) =>
+  arr.filter((ele) => ele.length < 7);
+
+const vowels = new Set(["a", "e", "i", "o", "u"]);
+const filterStartVowel = (arr: string[]) =>
+  arr.filter((ele) => vowels.has(ele[0]?.toLowerCase()!));
+
+const filter5vowels = (arr: string[]) =>
+  arr.filter((ele) => {
+    let count = 0;
+    Array.from(ele).forEach(
+      (char) => vowels.has(char.toLowerCase()) && count++,
+    );
+    return count >= 5;
+  });
+
+const filter1DistinctVowel = (arr: string[]) =>
+  arr.filter((ele) => {
+    const distinctVowls = new Set();
+    Array.from(ele).forEach(
+      (char) =>
+        vowels.has(char.toLowerCase()) && distinctVowls.add(char.toLowerCase()),
+    );
+    return distinctVowls.size === 1;
+  });
+
+const multiFilter = (arr: Record<string, string>[]) =>
+  arr.filter(
+    (ele) =>
+      ele["capital"]?.length! >= 8 &&
+      !vowels.has(ele["name"]![0]!.toLowerCase()) &&
+      Array.from(ele["tag"]!).filter((char) => vowels.has(char.toLowerCase()))
+        .length >= 1 &&
+      ele["region"] !== "South",
+  );
