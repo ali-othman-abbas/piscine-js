d473236055e3b34b188ead16fc5a905fc19fb801
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-18 13:29:10 +0300

    m

--- for-each.js
+++ for-each.js
@@ -1,6 +1,5 @@
 function forEach(arr, func) {
-    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
-        var ele = arr_1[_i];
-        func(ele);
+    for (var i = 0; i < arr.length; i++) {
+        func(arr[i], i, arr);
     }
 }
--- for-each.ts
+++ for-each.ts
@@ -1,5 +1,5 @@
-function forEach<T>(arr: T[], func: (x:T) => void) {
-  for (const ele of arr) {
-    func(ele)
+function forEach<T>(arr: T[], func: (x:T, idx: number, arr: T[]) => void) {
+  for (let i = 0; i < arr.length; i++) {
+    func(arr[i]!, i, arr)
   }
 }
