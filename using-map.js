8269ae83ce3bf4d8ddc79c66ad571470a3360ddd
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-18 03:50:08 +0300

    m

--- using-map.js
+++ using-map.js
@@ -2,24 +2,28 @@ var citiesOnly = function (arr) {
     return arr.map(function (ele) { return ele["city"]; });
 };
 function upperCasingStates(arr) {
-    return arr.map(function (ele) { return ele.split(" ").map(function (subEle) { return subEle.toUpperCase().charAt(0) + subEle.slice(1); }).join(" "); });
+    return arr.map(function (ele) {
+        return ele
+            .split(" ")
+            .map(function (subEle) { return subEle.toUpperCase().charAt(0) + subEle.slice(1); })
+            .join(" ");
+    });
 }
 function fahrenheitToCelsius(arr) {
     return arr.map(function (ele) {
         var num = Number(ele.slice(0, -2));
-        return "".concat(Math.round((5 / 9) * (num - 32)), "\u00B0C");
+        return "".concat(Math.floor((5 / 9) * (num - 32)), "\u00B0C");
     });
 }
 function trimTemp(arr) {
     return arr.map(function (ele) {
         var _a;
-        ele['temperature'] = (_a = ele['temperature']) === null || _a === void 0 ? void 0 : _a.trim();
+        ele["temperature"] = (_a = ele["temperature"]) === null || _a === void 0 ? void 0 : _a.trim();
         return ele;
     });
 }
 function tempForecasts(arr) {
-    var trimmed = trimTemp(arr);
-    var deg = fahrenheitToCelsius(trimmed.map(function (ele) { return ele["temperature"]; }));
-    var cap = upperCasingStates(trimmed.map(function (ele) { return ele["state"]; }));
-    return trimmed.map(function (ele, idx) { return "".concat(deg[idx], "elsius in ").concat(ele["city"], ", ").concat(cap[idx]); });
+    var deg = fahrenheitToCelsius(arr.map(function (ele) { var _a; return "".concat((_a = ele["temperature"]) === null || _a === void 0 ? void 0 : _a.match(/-?\d+/)[0], "\u00B0F"); }));
+    var cap = upperCasingStates(arr.map(function (ele) { return ele["state"]; }));
+    return arr.map(function (ele, idx) { return "".concat(deg[idx], "elsius in ").concat(ele["city"], ", ").concat(cap[idx]); });
 }
--- using-map.ts
+++ using-map.ts
@@ -2,33 +2,34 @@ const citiesOnly = (arr: Record<string, string>[]) =>
   arr.map((ele) => ele["city"]);
 
 function upperCasingStates(arr: string[]) {
-  return arr.map(
-    (ele) => ele.split(" ").map(
-      (subEle) => subEle.toUpperCase().charAt(0) + subEle.slice(1)
-    ).join(" ")
+  return arr.map((ele) =>
+    ele
+      .split(" ")
+      .map((subEle) => subEle.toUpperCase().charAt(0) + subEle.slice(1))
+      .join(" "),
   );
 }
 
 function fahrenheitToCelsius(arr: string[]) {
   return arr.map((ele) => {
     const num = Number(ele.slice(0, -2));
-    return `${Math.round((5 / 9) * (num - 32))}°C`;
+    return `${Math.floor((5 / 9) * (num - 32))}°C`;
   });
 }
 
 function trimTemp(arr: Record<string, string>[]) {
   return arr.map((ele) => {
-    ele['temperature'] = ele['temperature']?.trim()!
-    return ele
+    ele["temperature"] = ele["temperature"]?.trim()!;
+    return ele;
   });
 }
 
 function tempForecasts(arr: Record<string, string>[]) {
-  const trimmed = trimTemp(arr)
-  const deg = fahrenheitToCelsius(trimmed.map((ele) => ele["temperature"]!));
-  const cap = upperCasingStates(trimmed.map((ele) => ele["state"]!));
-  return trimmed.map(
+  const deg = fahrenheitToCelsius(
+    arr.map((ele) => `${ele["temperature"]?.match(/-?\d+/)![0]}°F`),
+  );
+  const cap = upperCasingStates(arr.map((ele) => ele["state"]!));
+  return arr.map(
     (ele, idx) => `${deg[idx]}elsius in ${ele["city"]}, ${cap[idx]}`,
   );
 }
-
