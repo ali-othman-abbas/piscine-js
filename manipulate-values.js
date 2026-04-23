35d3fb4e9d4abb7ca35a1f025c0944a085119c02
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-23 16:25:49 +0300

    m

---
+++ manipulate-values.js
@@ -0,0 +1,33 @@
+
+/**
+ * @template T
+ * @param {Record<string, T>} obj
+ * @param {(x: T) => boolean} func
+ * @returns {Record<string, T>}
+ */
+function filterValues(obj, func) {
+  return Object.fromEntries(Object.entries(obj).filter(([_, v]) => func(v)));
+}
+
+/**
+ * @template T
+ * @param {Record<string, T>} obj
+ * @param {(x: T) => T} func
+ * @returns
+ */
+function mapValues(obj, func) {
+  return Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, func(v)]));
+}
+
+/**
+ * @template T
+ * @param {Record<string, T>} obj
+ * @param {(prev: T, curr: T) => T} func
+ * @param T
+ */
+function reduceValues(obj, func) {
+  return Object.entries(obj).reduce(([_, accum], [__, curr]) => [
+    "",
+    func(accum, curr),
+  ])[1];
+}
