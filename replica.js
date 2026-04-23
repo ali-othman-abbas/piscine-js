d26a83afdcd85a8c741722a9b38e823c880a3d74
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-23 22:02:45 +0300

    m

--- replica.js
+++ replica.js
@@ -28,11 +28,12 @@ function replica(...obj) {
     return deepCopy(obj[0])
   }
   const mid = Math.floor(obj.length / 2)
-  const h1 = obj.slice(0, mid)
-  const h2 = obj.slice(mid)
+  const h1 = replica(...obj.slice(0, mid))
+  const h2 = replica(...obj.slice(mid))
   for (const k in h2) {
     h1[k] = h2[k]
   }
   
   return h1
 }
+
