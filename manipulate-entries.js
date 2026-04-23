4c725423e2f8654a0da229dae1f1afe33f3643a2
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-23 18:28:36 +0300

    m

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -1,55 +1,55 @@
-// const nutritionDB = {
-//   tomato: {
-//     calories: 18,
-//     protein: 0.9,
-//     carbs: 3.9,
-//     sugar: 2.6,
-//     fiber: 1.2,
-//     fat: 0.2,
-//   },
-//   vinegar: {
-//     calories: 20,
-//     protein: 0.04,
-//     carbs: 0.6,
-//     sugar: 0.4,
-//     fiber: 0,
-//     fat: 0,
-//   },
-//   oil: { calories: 48, protein: 0, carbs: 0, sugar: 123, fiber: 0, fat: 151 },
-//   onion: { calories: 0, protein: 1, carbs: 9, sugar: 0, fiber: 0, fat: 0 },
-//   garlic: {
-//     calories: 149,
-//     protein: 6.4,
-//     carbs: 33,
-//     sugar: 1,
-//     fiber: 2.1,
-//     fat: 0.5,
-//   },
-//   paprika: {
-//     calories: 282,
-//     protein: 14.14,
-//     carbs: 53.99,
-//     sugar: 1,
-//     fiber: 0,
-//     fat: 12.89,
-//   },
-//   sugar: {
-//     calories: 387,
-//     protein: 0,
-//     carbs: 100,
-//     sugar: 100,
-//     fiber: 0,
-//     fat: 0,
-//   },
-//   orange: {
-//     calories: 49,
-//     protein: 0.9,
-//     carbs: 13,
-//     sugar: 9,
-//     fiber: 0.2,
-//     fat: 0.1,
-//   },
-// };
+const nutritionDB = {
+  tomato: {
+    calories: 18,
+    protein: 0.9,
+    carbs: 3.9,
+    sugar: 2.6,
+    fiber: 1.2,
+    fat: 0.2,
+  },
+  vinegar: {
+    calories: 20,
+    protein: 0.04,
+    carbs: 0.6,
+    sugar: 0.4,
+    fiber: 0,
+    fat: 0,
+  },
+  oil: { calories: 48, protein: 0, carbs: 0, sugar: 123, fiber: 0, fat: 151 },
+  onion: { calories: 0, protein: 1, carbs: 9, sugar: 0, fiber: 0, fat: 0 },
+  garlic: {
+    calories: 149,
+    protein: 6.4,
+    carbs: 33,
+    sugar: 1,
+    fiber: 2.1,
+    fat: 0.5,
+  },
+  paprika: {
+    calories: 282,
+    protein: 14.14,
+    carbs: 53.99,
+    sugar: 1,
+    fiber: 0,
+    fat: 12.89,
+  },
+  sugar: {
+    calories: 387,
+    protein: 0,
+    carbs: 100,
+    sugar: 100,
+    fiber: 0,
+    fat: 0,
+  },
+  orange: {
+    calories: 49,
+    protein: 0.9,
+    carbs: 13,
+    sugar: 9,
+    fiber: 0.2,
+    fat: 0.1,
+  },
+};
 /**
  * @template T
  * @param {Record<string, T>} obj
@@ -80,10 +80,9 @@ function reduceEntries(obj, func, accum) {
 
 function totalCalories(obj) {
   obj = Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, v / 100]));
-  console.log(obj)
   return reduceEntries(
     obj,
-    (accum, [k, v]) => Number((accum + nutritionDB[k]["calories"] * v).toFixed(2)),
+    (accum, [k, v]) => Number((accum + nutritionDB[k]["calories"] * v).toFixed(6)),
     0,
   );
 }
@@ -101,16 +100,16 @@ function cartTotal(obj) {
     k,
     mapEntries(nutritionDB[k], ([_k, _v]) => [
       _k,
-      Number((_v * v).toFixed(2)),
+      Number((_v * v).toFixed(6)),
     ]),
   ]);
 }
 
-// const groceriesCart = { orange: 500, oil: 20, sugar: 480 }
+const groceriesCart = { oil: 5, onion: 2.3, garlic: 2.2, paprika: 4.8 }
 
-// console.log('Total calories:')
-// console.log(totalCalories(groceriesCart))
-// console.log('Items with low carbs:')
-// console.log(lowCarbs(groceriesCart))
-// console.log('Total cart nutritional facts:')
-// console.log(cartTotal(groceriesCart))
+console.log('Total calories:')
+console.log(totalCalories(groceriesCart))
+console.log('Items with low carbs:')
+console.log(lowCarbs(groceriesCart))
+console.log('Total cart nutritional facts:')
+console.log(cartTotal(groceriesCart))
