fabe431047c7721094f779849ead3aecffc6a152
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-23 20:38:58 +0300

    m

---
+++ deep-copy.js
@@ -0,0 +1,20 @@
+function deepCopy(obj) {
+  let isArr = false
+  if (Array.isArray(obj)) {
+    isArr = true
+  }
+  const copy = {}
+  for (const k in obj) {
+    if (Array.isArray(obj[k]) || obj[k]?.constructor === 'Object') {
+      copy[k] = deepCopy(obj[k])
+    } else {
+      copy[k] = obj[k]
+    }
+  }
+  
+  if (isArr) {
+    return Object.entries(copy).map(([_, v]) => v)
+  }
+  
+  return copy
+}
