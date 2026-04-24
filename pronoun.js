c0ffce552b5af0e16321768648ab0b5eaa30eb9b
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-24 19:05:34 +0300

    m

--- pronoun.js
+++ pronoun.js
@@ -6,12 +6,12 @@
 function pronoun(str) {
   const wordRe = /\b\w+\b/g;
   const wordArr = str.match(wordRe);
+  console.log(wordArr)
   const pronouns = ["i", "you", "she", "it", "they", "we"];
 
   const result = {};
   for (let i = 0; i < wordArr.length - 1; i++) {
     if (
-      i < wordArr.length - 1 &&
       pronouns.includes(wordArr[i].toLowerCase()) &&
       !pronouns.includes(wordArr[i + 1])
     ) {
@@ -19,10 +19,11 @@ function pronoun(str) {
       if (!(pro in result)) {
         result[pro] = { word: [], count: 0 };
       }
-      result[pro].word.push(wordArr[i] + 1);
+      result[pro].word.push(wordArr[i + 1]);
       result[pro].count++;
     }
   }
-  
-  return result
+
+  return result;
 }
+
