11f131341ee4cacc85fa62ddd4abcb9299a0f7e7
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-24 20:21:30 +0300

    m

--- neuron.js
+++ neuron.js
@@ -6,29 +6,27 @@
 function neuron(sentences) {
   const extractRE = /^(\w+):(.*)-\s*Response:(.*)$/
   const result = {}
-  sentences.forEach(
-    s => {
-      const groups = s.match(extractRE).slice(1)
-      const command = groups[0].toLowerCase()
-      const commandSingular = command.slice(0, -1)
-      const commandArg = groups[1].trim()
-      const commandArgProp = commandArg.toLowerCase()
-        .replace(/\s/g, '_')
-        .replace(/\W/g, '')
-      const response = groups[2].trim()
-      
-      if (!(command in result)) {
-        result[command] = {}
-      }
-      if (!(commandArgProp in result[command])) {
-        result[command][commandArgProp] = {
-          responses: []
-        }
-        result[command][commandArgProp][commandSingular] = commandArg
-      }
-      result[command][commandArgProp].responses.push(response)
+  for (const s in sentences) {
+    const groups = s.match(extractRE).slice(1)
+    const command = groups[0].toLowerCase()
+    const commandSingular = command.slice(0, -1)
+    const commandArg = groups[1].trim()
+    const commandArgProp = commandArg.toLowerCase()
+      .replace(/\s/g, '_')
+      .replace(/\W/g, '')
+    const response = groups[2].trim()
+    
+    if (!(command in result)) {
+      result[command] = {}
     }
-  )
+    if (!(commandArgProp in result[command])) {
+      result[command][commandArgProp] = {
+        responses: []
+      }
+      result[command][commandArgProp][commandSingular] = commandArg
+    }
+    result[command][commandArgProp].responses.push(response)
+  }
   
   return result
 }
