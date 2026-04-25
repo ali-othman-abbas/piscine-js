cddadbb9a9c38929e5bce83c71e1fb56847aae96
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-25 11:37:56 +0300

    m

---
+++ gossip-grid.js
@@ -0,0 +1,106 @@
+import { gossips } from "./gossip-grid.data.js";
+export function grid() {
+  const body = document.body;
+  const grid = buildGrid();
+  body.append(grid);
+  const ranges = buildRanges();
+
+  body.prepend(ranges)
+}
+
+function buildRanges() {
+  const sliders = document.createElement("div");
+  sliders.classList.add('ranges')
+  
+  const widthSlider = document.createElement("input");
+  const fontSizeSlider = document.createElement("input");
+  const backgroundSlider = document.createElement("input");
+  
+  
+  widthSlider.type = 'range'
+  fontSizeSlider.type = 'range'
+  backgroundSlider.type = 'range'
+  
+  
+  widthSlider.id = 'width'
+  fontSizeSlider.id = 'fontSize'
+  backgroundSlider.id = 'background'
+  
+  widthSlider.classList.add('range')
+  fontSizeSlider.classList.add('range')
+  backgroundSlider.classList.add('range')
+  
+  widthSlider.min = "200";
+  widthSlider.max = "800";
+  fontSizeSlider.min = "20";
+  fontSizeSlider.max = "40";
+  backgroundSlider.min = "20";
+  backgroundSlider.max = "75";
+  
+  
+  /**
+   * @type {NodeListOf<HTMLElement>}
+   */
+  const allGossips = document.querySelectorAll(`div.gossip`)
+  const style = getComputedStyle(allGossips[0])
+  widthSlider.value = `${parseInt(style.width)}`
+  fontSizeSlider.value = `${parseInt(style.fontSize)}`
+  backgroundSlider.value = `50`
+  widthSlider.addEventListener('input', (_) => {
+    allGossips.forEach(ele => ele.style.width = `${widthSlider.value}px`)
+  })
+  fontSizeSlider.addEventListener('input', (_) => {
+    allGossips.forEach(ele => ele.style.fontSize = `${fontSizeSlider.value}px`)
+  })
+  backgroundSlider.addEventListener('input', (_) => {
+    allGossips.forEach(ele => ele.style.backgroundColor = `hsl(280, 50%, ${backgroundSlider.value}%)`)
+  })
+  
+  sliders.append(widthSlider, fontSizeSlider, backgroundSlider)
+  return sliders
+}
+
+/**
+ * @return {HTMLElement}
+ */
+function buildGrid() {
+  const grid = document.createElement("div");
+  grid.classList.add("gossip-grid");
+  for (const g of gossips) {
+    const div = document.createElement("div");
+    div.classList.add("gossip");
+    div.textContent = g;
+    grid.append(div);
+  }
+  const form = buildForm(grid);
+
+  const formWrapper = document.createElement("div");
+  formWrapper.classList.add("gossip");
+  formWrapper.append(form);
+  grid.prepend(formWrapper);
+
+  return grid;
+}
+
+/**
+ * @param {HTMLElement} grid
+ * @return {HTMLElement}
+ */
+function buildForm(grid) {
+  const form = document.createElement("form");
+  form.classList.add("gossip");
+  const textArea = document.createElement("textarea");
+  const button = document.createElement("button");
+  button.textContent = "Share gossip!";
+  button.type = "submit";
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const div = document.createElement("div");
+    div.textContent = textArea.value;
+    div.classList.add("gossip");
+    grid.append(div);
+    textArea.value = ''
+  });
+  form.append(textArea, button);
+  return form;
+}
