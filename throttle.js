e20e4b9c65a536869d284e7499a94f34ffb3c76c
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-26 16:44:44 +0300

    m

--- throttle.js
+++ throttle.js
@@ -1,25 +1,43 @@
+
+const add = (arr, el) => arr?.push(el)
+const run = (callback, callLimit, nbr) =>
+  new Promise((r) => {
+    let arr = []
+    let inter = setInterval(() => {
+      callback(arr, 1)
+    }, callLimit)
+    setTimeout(() => {
+      clearInterval(inter)
+      r(arr.length)
+    }, callLimit * nbr)
+  })
+
 function throttle(func, wait) {
-  let firstCall = true
-  let callInWindow = false
+  let firstCall = true;
+  let callInWindow = false;
   let timeout = null;
-  let lastArgs = null
+  let lastArgs = null;
+
   return (...args) => {
-    lastArgs = args
-    if (!firstCall && !callInWindow) {
-      callInWindow = true
-    }
+    lastArgs = args;
+
     if (firstCall) {
-      func(...lastArgs)
-      firstCall = false
+      func(...args);
+      firstCall = false;
+    } else {
+      callInWindow = true;
     }
+
     if (timeout === null) {
       timeout = setTimeout(() => {
         if (callInWindow) {
-          func(...lastArgs)
-          timeout.refresh()
-          callInWindow = false
+          func(...lastArgs);
         }
-      }, wait)
+
+        // reset for next window
+        callInWindow = false;
+        timeout = null;
+      }, wait);
     }
   };
 }
@@ -80,13 +98,21 @@ function leadingThrottle(func, wait) {
   };
 }
 
-// const fun = throttle((i) => console.log(i), 2000)
+const fun = throttle((i) => console.log(i), 100)
 
-// let i = 0
-// setInterval(
-//   () => {
-//     fun(i)
-//     i++
-//   },
-//   100
-// )
+let i = 0
+setInterval(
+  () => {
+    fun(i)
+    i++
+  },
+  100
+)
+
+// async function hello() {
+  
+//   const result = await run(throttle(add, 100), 26, 5)
+//   console.log(result)
+// }
+
+// hello()
