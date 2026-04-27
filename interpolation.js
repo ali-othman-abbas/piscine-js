b9e9560d13c18d06d391a32b42d45d1776d44adf
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-27 17:58:14 +0300

    m

--- interpolation.js
+++ interpolation.js
@@ -2,13 +2,13 @@ function interpolation({ step, start, end, callback, duration }) {
   const mov = (end - start) / step;
 
   let i = 1;
-  let x = start + step * i;
-  let y = duration * (step * (i + 1));
+  let x = start + mov * i;
+  let y = duration * (mov * (i + 1));
   const id = setInterval(() => {
     callback([x, y]);
     i++;
-    x = start + step * i;
-    y = duration * (step * (i + 1));
+    x = start + mov * i;
+    y = duration * (mov * (i + 1));
     if (x === end) id.close();
   }, y);
 }
