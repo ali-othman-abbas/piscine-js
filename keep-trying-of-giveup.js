26441a38c6c41710055c7e5277e9f023e09d90e4
ali-othman-abbas <ali.collage.student@gmail.com>
2026-04-27 19:41:52 +0300

    m

---
+++ keep-trying-of-giveup.js
@@ -0,0 +1,42 @@
+/**
+ * @template T, U
+ * @param {number} count
+ * @param {(...args: T[]) => Promise<U>} callback
+ * @return {(...args: T[]) => Promise<U>}
+ */
+function retry(count, callback) {
+  let errCnt = 0;
+  return async (...args) => {
+    try {
+      return await callback(...args);
+    } catch (err) {
+      errCnt++;
+      if (errCnt >= count) {
+        throw new Error("fail");
+      }
+    }
+  };
+}
+
+/**
+ * @template T, U
+ * @param {number} delay
+ * @param {(...args: T[]) => Promise<U>} callback
+ * @returns {(...args: T[]) => Promise<U>}
+ */
+function timeout(delay, callback) {
+  return async (...args) => {
+    const prom = new Promise(async (resolve, reject) => {
+      setTimeout(() => reject(new Error("delayed")), delay);
+      resolve(await callback(...args));
+    });
+
+    try {
+      return await prom;
+    } catch (err) {
+      throw err;
+    }
+  };
+}
+
+
