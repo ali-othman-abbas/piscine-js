d16294392bb942293e34aa2aa497914c95292fa2
ali-othman-abbas <ali.collage.student@gmail.com>
2026-05-03 00:32:32 +0300

    m

--- series.js
+++ series.js
@@ -1,3 +1,18 @@
 function series(arr) {
-  return Promise.all(arr)
+  const results = Array.from({ length: arr.length });
+  let remaining = arr.length;
+  return new Promise((res, _) => {
+    function dec() {
+      remaining--;
+      if (remaining === 0) {
+        res(results);
+      }
+    }
+    arr.forEach((func, idx) => {
+      func().then((val) => {
+        results[idx] = val;
+        dec();
+      });
+    });
+  });
 }
