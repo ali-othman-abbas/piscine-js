0e875b3849e9aeeed4bd88d9e14b79fa9d8eaf29
ali-othman-abbas <ali.collage.student@gmail.com>
2026-05-02 18:13:10 +0300

    m

--- all.js
+++ all.js
@@ -7,17 +7,35 @@ async function all(obj) {
   if (Object.keys(obj).length === 0) {
     return Promise.resolve({});
   }
-  const arr = Object.entries(obj).map(([_, p]) => p);
   let counter = 0;
   let failed = null;
+  
+  let resolveFn;
+  let rejFn;
+  const promise = new Promise((res, rej) => {
+    resolveFn = res;
+    rejFn = rej;
+  });
+  
+  function check() {
+    if (failed !== null) {
+      rejFn(failed);
+    }
+    if (counter === arr.length) {
+      resolveFn();
+    }
+  }
 
-  const promiseArr = arr.map((p) => {
+  const arr = Object.entries(obj).map(([_, p]) => p);
+  const resolvedArr = Array.from({length: arr.length})
+  
+  arr.forEach((p, idx) => {
     p = Promise.resolve(p);
     return p.then(
       (val) => {
         counter++;
         check();
-        return val;
+        resolvedArr[idx] = val
       },
       (err) => {
         failed = err;
@@ -26,24 +44,10 @@ async function all(obj) {
     );
   });
 
-  let resolveFn;
-  let rejFn;
-  const promise = new Promise((res, rej) => {
-    resolveFn = res;
-    rejFn = rej;
-  });
 
-  function check() {
-    if (failed !== null) {
-      rejFn(failed);
-    }
-    if (counter === arr.length) {
-      resolveFn(promiseArr);
-    }
-  }
 
-  const returnedPromiseArr = await promise;
+  await promise;
   return Object.fromEntries(
-    Object.entries(obj).map(([str, _], idx) => [str, returnedPromiseArr[idx]]),
+    Object.entries(obj).map(([str, _], idx) => [str, resolvedArr[idx]]),
   );
 }
