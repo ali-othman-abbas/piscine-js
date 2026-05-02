194d7f7daecfd1191cdbdc52f49921edc022db6b
ali-othman-abbas <ali.collage.student@gmail.com>
2026-05-03 00:54:11 +0300

    m

--- series.js
+++ series.js
@@ -4,25 +4,28 @@
  * @returns 
  */
 function series(arr) {
-  const results = Array.from({ length: arr.length });
-  let remaining = arr.length;
-  return new Promise((res, _) => {
-    if (arr.length === 0) {
-      res([])
-    }
-    function dec() {
-      remaining--;
-      if (remaining === 0) {
-        res(results);
-      }
-    }
-    arr.forEach((func, idx) => {
-      func().then((val) => {
-        results[idx] = val;
-        dec();
-      });
-    });
-  });
+  return Promise.all(
+    arr.map(func => Promise.resolve(func()))
+  );
+  // const results = Array.from({ length: arr.length });
+  // let remaining = arr.length;
+  // return new Promise((res, _) => {
+  //   if (arr.length === 0) {
+  //     res([])
+  //   }
+  //   function dec() {
+  //     remaining--;
+  //     if (remaining === 0) {
+  //       res(results);
+  //     }
+  //   }
+  //   arr.forEach((func, idx) => {
+  //     Promise.resolve(func()).then((val) => {
+  //       results[idx] = val;
+  //       dec();
+  //     });
+  //   });
+  // });
 }
 
 // function wait(ms) {
@@ -30,7 +33,7 @@ function series(arr) {
 //     new Promise((res, _) => setTimeout(res, ms, ms));
 // }
 
-// const arr = [];
+// const arr = [wait(1000), wait(2000), wait(10)];
 
 //   (async () => {
 //     const start = Date.now()
