74425b41926aaae2b679e6fc968c0e5964b1bc56
ali-othman-abbas <ali.collage.student@gmail.com>
2026-05-03 18:54:52 +0300

    m

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -4,12 +4,11 @@ import { argv } from "process";
 async function list(path) {
   try {
     const files = await readdir(path)
-    const result = files
+    files
       .map(el => el.split('.')[0].split('_'))
       .toSorted((a, b) => a[1].localeCompare(b[1]))
       .map(([first, last], idx) => `${idx + 1}. ${last} ${first}` )
       .forEach(el => console.log(el))
-    
     console.log(result)
   } catch (err) {
     console.log(err)
