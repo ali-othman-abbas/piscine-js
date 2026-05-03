fb5c15d592424c6e2751ecb5366a06d734577cbb
ali-othman-abbas <ali.collage.student@gmail.com>
2026-05-03 20:48:10 +0300

    m

--- tell-it-cypher.mjs
+++ tell-it-cypher.mjs
@@ -1,6 +1,5 @@
-import { readdir, readFile, writeFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 import { argv } from "process";
-import { buffer } from "stream/consumers";
 
 const path = argv[2];
 const keyword = argv[3];
@@ -8,17 +7,17 @@ let distination = argv[4];
 
 (async function () {
   try {
-    const content = await readFile(path, 'utf8');
-    if (keyword === 'encode') {
-      const buf = Buffer.from(content, 'utf8')
-      const outputName = distination ? distination : 'cypher.txt'
-      await writeFile(outputName, buf.toString('base64'))
+    const content = await readFile(path, "utf8");
+    if (keyword === "encode") {
+      const buf = Buffer.from(content, "utf8");
+      const outputName = distination ? distination : "cypher.txt";
+      await writeFile(outputName, buf.toString("base64"));
     } else {
-      const buf = Buffer.from(content, 'base64')
-      const outputName = distination ? distination : 'clear.txt'
-      await writeFile(outputName, buf.toString('utf8'))
+      const buf = Buffer.from(content, "base64");
+      const outputName = distination ? distination : "clear.txt";
+      await writeFile(outputName, buf.toString("utf8"));
     }
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 })();
