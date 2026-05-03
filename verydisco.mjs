01ff5d17f5ebfc44e68b0f113cbe9cbdde81758c
ali-othman-abbas <ali.collage.student@gmail.com>
2026-05-03 14:45:16 +0300

    m

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,12 +1,13 @@
-/**
- *
- * @type {(str: string) => string}
- */
-const verydisco = (str) =>
-  str
-    .split(" ")
-    .map((el) => {
-      const mid = Math.ceil(el.length/2)
-      return [el.slice(mid), el.slice(0, mid)].join("");
-    })
-    .join(" ");
+import { argv } from "process";
+
+ 
+ const str = argv[2]
+ const result = str
+   .split(" ")
+   .map((el) => {
+     const mid = Math.ceil(el.length/2)
+     return [el.slice(mid), el.slice(0, mid)].join("");
+   })
+   .join(" ");
+
+ console.log(result)
