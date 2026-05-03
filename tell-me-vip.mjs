560c121f19c38571319c2e4449853f4b343750b7
ali-othman-abbas <ali.collage.student@gmail.com>
2026-05-03 19:47:43 +0300

    m

---
+++ tell-me-vip.mjs
@@ -0,0 +1,26 @@
+import { readdir, readFile } from "fs/promises";
+import { argv } from "process";
+
+async function filterForYes(path) {
+  try {
+    const file = await readdir(path);
+    const promiseMap = file.map((el) =>
+      readFile(`${path}/${el}`, {
+        encoding: "utf8",
+      }),
+    );
+    const jsons = await Promise.all(promiseMap);
+    file
+      .filter((_, idx) => JSON.parse(jsons[idx]).answer === "yes")
+      .map((el) => el.split(".")[0].split("_"))
+      .toSorted((a, b) => a[1].localeCompare(b[1]))
+      .map(([first, last], idx) => `${idx + 1}. ${last} ${first}`)
+      .forEach((el) => console.log(el));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const path = argv[2]
+
+filterForYes(path);
